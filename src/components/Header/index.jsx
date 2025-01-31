0e9748906bf2e58fd0d5f707200bd3214bfcc7cc
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2025-01-31 14:04:04 -0300

    feat: create header nav and hidden nav, create and add styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,34 @@
 import NameLogo from "../../assets/NameLogo.svg";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import styles from "./style.module.scss";
+import { useState } from "react";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="section-black">
       <div className="container">
-        <img src={NameLogo} alt="Name Logo" />
-        <button>
-          <GiHamburgerMenu size={21} />
-        </button>
+        <div className={styles.headerBox}>
+          <img src={NameLogo} alt="Name Logo" />
+          <button onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? <IoClose size={24} /> : <GiHamburgerMenu size={21} />}
+          </button>
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">Projetos</a>
+            <a href="#">Contato</a>
+            <a href="#">Mídias Digitais</a>
+          </nav>
+        </div>
       </div>
+      <nav className={isOpen ? styles.open : styles.close}>
+        <a href="#">Home</a>
+        <a href="#">Projetos</a>
+        <a href="#">Contato</a>
+        <a href="#">Mídias Digitais</a>
+      </nav>
     </header>
   );
 }
